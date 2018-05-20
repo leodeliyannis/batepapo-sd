cba70e5ddd698b8d05c12854248a374d085681ec
febat <33875964+febat@users.noreply.github.com>
2018-05-20 19:55:23 -0300

    Criação App

---
+++ ChatMobile/ChatMobile/ChatMobile/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatMobile.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
