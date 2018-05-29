53d938dc61da7d70567db29fb624425e5ba0798a
febat <33875964+febat@users.noreply.github.com>
2018-05-29 15:42:10 -0300

    adição tela de chat

--- ChatMobile/ChatMobile/ChatMobile/Views/BuscaPage.xaml.cs
+++ ChatMobile/ChatMobile/ChatMobile/Views/BuscaPage.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
@@ -12,9 +16,22 @@ namespace ChatMobile
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class BuscaPage : ContentPage
 	{
-		public BuscaPage ()
+        TcpClient TCP { get; set; }
+        NetworkStream Stream { get; set; }
+        StreamWriter StreamWriter { get; set; }
+
+        Thread Resposta { get; set; }
+        public BuscaPage ()
 		{
 			InitializeComponent ();
-		}
-	}
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TCP = new TcpClient();
+            TCP.Connect(ip, 8080);
+            App.Conversando = true;
+            Stream = TCP.GetStream();
+            StreamWriter = new StreamWriter(TCP.GetStream());
+            StreamWriter.AutoFlush = true;
+
+        }
+    }
 }
